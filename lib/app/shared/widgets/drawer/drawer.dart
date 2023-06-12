f3bc799a8e351f06de19c33985c9300ec6a96479
stobertonf <stobertonf@gmail.com>
2023-06-12 17:40:43 -0300

    feat(Task-Page): Changing Custon Drawer File.

--- lib/app/shared/widgets/drawer/drawer.dart
+++ lib/app/shared/widgets/drawer/drawer.dart
@@ -1,51 +1,101 @@
 import 'package:flutter/material.dart';
 import 'package:status_management_list/app/pages/home/home_page.dart';
+import 'package:status_management_list/app/pages/tasks/task_page.dart';
 
 class CustonDrawer extends StatelessWidget {
-  const CustonDrawer({super.key});
+  const CustonDrawer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        children: [
-          ListTile(
-            leading: const Icon(Icons.task),
-            title: const Text("Tarefas"),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => const HomePage(),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.black,
+              child: UserAccountsDrawerHeader(
+                accountName: const Text(
+                  "stoberton.francisco",
                 ),
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.chat),
-            title: const Text("Chat"),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => const HomePage(),
+                accountEmail: const Text(
+                  "stoberton.francisco@smfsystems.com.br",
                 ),
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.bug_report),
-            title: const Text("Tasks"),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => const HomePage(),
+                decoration: const BoxDecoration(
+                  color: Colors.black,
                 ),
-              );
-            },
-          ),
-        ],
+                currentAccountPicture: SizedBox(
+                  child: Image.asset(
+                    'assets/images/smf_logo.png',
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.bug_report,
+              ),
+              title: const Text(
+                "Bugs",
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const HomePage(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.category,
+              ),
+              title: const Text(
+                "Categories",
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const HomePage(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.task,
+              ),
+              title: const Text(
+                "Tasks",
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const TasksPage(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.people,
+              ),
+              title: const Text(
+                "Usuários",
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const HomePage(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
